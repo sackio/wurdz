134a4688911c7df240006af41150d8886bfe53e7
Ben Sack <ben@sack.io>
2022-07-17 04:42:19 -0400

    latest commit

---
+++ Dockerfile
@@ -0,0 +1,68 @@
+FROM node:16.5
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=America/New_York
+
+RUN echo $TZ > /etc/timezone && \
+    apt-get update && apt-get install -y tzdata && \
+    rm /etc/localtime && \
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
+    dpkg-reconfigure -f noninteractive tzdata && \
+    apt-get clean
+
+RUN apt-get update -y
+RUN apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg \
+    lsb-release \
+    python3-pip \
+    curl \
+    wget \
+    openvpn \
+    openresolv \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libfontconfig1 \
+    libgbm1 \
+    libgcc1 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxss1 \
+    libxtst6 \
+    xdg-utils 
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+
+RUN apt-get install dumb-init
+
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+CMD ["npm", "run", "start"]
---
+++ scripts/solver.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+
+const  _ = require('lodash')
+    , Async = require('async')
+    , OS = require('os')
+    , Delay = require('delay')
+    , Path = require('path')
+    , FS = require('fs')
+    , FSE = require('fs-extra')
+    , Util = require('util')
+    , Moment = require('moment')
+    , CP = require('child_process')
+    , Words = FS.readFileSync(require('word-list')).toString().split('\n')
+    , Optionator = require('optionator')({
+        options: [
+          {
+            option: 'outpath'
+          , type: 'String'
+          , required: true
+          }
+        , {
+            option: 'minlength'
+          , type: 'Number'
+          , required: true
+          , default: '3'
+          }
+        , {
+            option: 'allshift'
+          , type: 'Boolean'
+          }
+        ]
+      })
+;
+
+var OPTS
+  , SCRIPT = Path.basename(__filename);
+
+try {
+  OPTS = Optionator.parseArgv(process.argv);
+} catch (err) {
+  console.log(Optionator.generateHelp());
+  process.exit();
+}
+
+(async function Main(){
+  let masks = {};
+  
+  Words.map(word => {
+    if (word.length < OPTS.minlength) return;
+    
+    let consonant_mask = word.replace(/(a|e|i|o|u)/g, '_');
+    if (!consonant_mask.match(/\_/)) return;
+    
+    let vowel_mask = word.replace(/[^aeiou]/g, '');
+    
+    masks[consonant_mask] = masks[consonant_mask] || {
+      consonant_mask
+    , word_length: word.length
+    , vowel_count: vowel_mask.length
+    , words: []
+    };
+    
+    masks[consonant_mask].words.push({
+      word
+    , vowel_mask 
+    });
+  });
+  
+  let solutions = _.pickBy(masks, mask => {
+    if (mask.words.length !== 5) return false;
+    
+    let solution = true
+      , index = 0;
+      
+    while (solution && index < mask.vowel_count) {
+      let vowels = _.uniq(_.map(mask.words, word => word.vowel_mask[index]));
+      index++;
+      
+      if (vowels.length === 5) continue;
+      
+      if (vowels.length !== 1 || OPTS.allshift) {
+        solution = false;
+        break;
+      }
+    }
+    
+    if (solution) return true;
+  });
+  
+  let csv = _.map(solutions, mask => {
+    return `${mask.consonant_mask},${mask.words.length},${mask.vowel_count},`
+          + _.map(mask.words, 'word').join(',');
+  }).join('\n');
+  
+  FS.writeFileSync(OPTS.outpath, csv);
+})();
